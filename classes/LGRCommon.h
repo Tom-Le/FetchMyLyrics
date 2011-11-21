a8e4d5d0c10c00be7e67cc400b638744368410bd
Tom Cat <tomthecat94@gmail.com>
2011-11-21 21:21:33 +0700

    And I thought to myself, why the f*** not.
    My tweak is now licensed under the WTFPL.

--- classes/LGRCommon.h
+++ classes/LGRCommon.h
@@ -2,9 +2,11 @@
  * LGRCommon.h 
  * L'Fetcher
  *
- * Copyright 2011, Le Son.
- * All rights reserved.
- * Licensed under the BSD license, included with the source.
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details.
  ******************************************************************************/
 
 // For debugging (because they say NSLog is synchronous and all)
