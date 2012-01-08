71f98bac5ad9b876a34225fb18769f501646bb97
Tom Cat <tomthecat94@gmail.com>
2012-01-08 14:11:09 +0700

    Refactored FMLAZLyricsOperation.
    FMLAZLyricsPageParser is no longer needed. It will be missed.

--- classes/FMLAZLyricsOperation.m
+++ classes/FMLAZLyricsOperation.m
@@ -9,7 +9,6 @@
 
 #import "FMLAZLyricsOperation.h"
 
-#import "FMLAZLyricsPageParser.h"
 #import "FMLLyricsWrapper.h"
 #import "FMLController.h"
 #import "FMLCommon.h"
@@ -89,36 +88,59 @@
             return;
 
         NSError *error = NULL;
-        NSRegularExpression *regexForURL = [NSRegularExpression regularExpressionWithPattern:@"[^a-zA-Z0-9]*"
+        NSRegularExpression *nonAlphanumericRegex = [NSRegularExpression regularExpressionWithPattern:@"[^a-zA-Z0-9]*"
                                                                                      options:NSRegularExpressionCaseInsensitive
                                                                                        error:&error];
         if (error)
             return;
 
-        NSString *titleForURL = [regexForURL stringByReplacingMatchesInString:[self.title lowercaseString]
-                                                                      options:0
-                                                                        range:NSMakeRange(0, [self.title length])
-                                                                 withTemplate:@""];
-        NSString *artistForURL = [regexForURL stringByReplacingMatchesInString:[self.artist lowercaseString]
-                                                                       options:0
-                                                                         range:NSMakeRange(0, [self.artist length])
-                                                                  withTemplate:@""];
+        NSString *titleForURL = [nonAlphanumericRegex stringByReplacingMatchesInString:[self.title lowercaseString]
+                                                                               options:0
+                                                                                 range:NSMakeRange(0, [self.title length])
+                                                                          withTemplate:@""];
+        NSString *artistForURL = [nonAlphanumericRegex stringByReplacingMatchesInString:[self.artist lowercaseString]
+                                                                                options:0
+                                                                                  range:NSMakeRange(0, [self.artist length])
+                                                                           withTemplate:@""];
         NSString *URLStringToPage = [@"http://www.azlyrics.com/lyrics/" stringByAppendingFormat:@"%@/%@.html", artistForURL, titleForURL];
         NSURL *URLToPage = [NSURL URLWithString:URLStringToPage];
+
+        // SECOND STEP: Fetch lyrics
+        // Fetch lyrics page
+        NSData *data = [NSData dataWithContentsOfURL:URLToPage];
 
         // Periodic check.
         if ([self isCancelled])
             return;
 
-        // SECOND STEP: Fetch lyrics
-
-        // Set up parser
-        FMLAZLyricsPageParser *pageParser = [[[FMLAZLyricsPageParser alloc] init] autorelease];
-        pageParser.URLToPage = URLToPage;
-        // Fetch lyrics (woooooo)
-        NSString *lyrics = [pageParser lyricsFromParsing];
-        if (lyrics)
-            self.lyrics = [lyrics copy];
+        if (data)
+        {
+            NSString *pageHTML = [[[NSString alloc] initWithData:data
+                                                        encoding:NSUTF8StringEncoding] autorelease];
+            // The lyrics is conveniently located between two comments.
+            // \s stands for whitespace while \S stands for non-whitespace (dunno why I can't use . here)
+            NSRegularExpression *lyricsExtractionRegex = [NSRegularExpression regularExpressionWithPattern:@"(?:<!-- start of lyrics -->)([\\s\\S]*)(?:<!-- end of lyrics -->)"
+                                                                                                   options:(NSRegularExpressionCaseInsensitive||NSRegularExpressionDotMatchesLineSeparators)
+                                                                                                     error:nil];
+            __block NSString *untidiedLyrics = nil;
+            [lyricsExtractionRegex enumerateMatchesInString:pageHTML
+                                                    options:0
+                                                      range:NSMakeRange(0, [pageHTML length])
+                                                 usingBlock: ^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
+                                                     NSRange matchRange = [result rangeAtIndex:1];
+                                                     untidiedLyrics = [pageHTML substringWithRange:matchRange];
+                                                 }];
+            if (untidiedLyrics)
+            {
+                NSRegularExpression *tidyRegex = [NSRegularExpression regularExpressionWithPattern:@"(^\\s+)|(<.*>)|(\\s+$)"
+                                                                                           options:NSRegularExpressionCaseInsensitive
+                                                                                             error:nil];
+                self.lyrics = [tidyRegex stringByReplacingMatchesInString:untidiedLyrics
+                                                                  options:0
+                                                                    range:NSMakeRange(0, [untidiedLyrics length])
+                                                             withTemplate:@""];
+            }
+        }
     }
     @catch (id e)
     {
